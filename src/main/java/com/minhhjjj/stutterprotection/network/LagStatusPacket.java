79e3401f0d91fb44b37bfc2c6864038327c810a3
minhhjj5678 <83084666+minhhjj5678@users.noreply.github.com>
2026-02-24 22:42:04 +0700

    Update TYPE namespace from 'lagprotection' to 'stutterprotection'

--- src/main/java/com/minhhjjj/stutterprotection/network/LagStatusPacket.java
+++ src/main/java/com/minhhjjj/stutterprotection/network/LagStatusPacket.java
@@ -11,7 +11,7 @@ import net.neoforged.neoforge.network.registration.PayloadRegistrar;
 
 public class LagStatusPacket implements CustomPacketPayload {
     @SuppressWarnings("null")
-    public static final CustomPacketPayload.Type<LagStatusPacket> TYPE = new CustomPacketPayload.Type<>(ResourceLocation.fromNamespaceAndPath("lagprotection","payload"));
+    public static final CustomPacketPayload.Type<LagStatusPacket> TYPE = new CustomPacketPayload.Type<>(ResourceLocation.fromNamespaceAndPath("stutterprotection","payload"));
 	public static final LagStatusPacket PACKET = new LagStatusPacket();
 
     public LagStatusPacket() {}
